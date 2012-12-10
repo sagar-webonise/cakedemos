08d4a278888af371b81455e97ce0227d935bba4e
Sagar Shirsath <sagars@weboniselab.com>
2012-12-10 17:40:23 +0530

    cron added

---
+++ php/app/Test/Fixture/RoleFixture.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * RoleFixture
+ *
+ */
+class RoleFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'is_active' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 1),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'is_active' => 1,
+			'created' => '2012-12-10 17:30:15',
+			'modified' => '2012-12-10 17:30:15'
+		),
+	);
+
+}
